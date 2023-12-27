076fdaa2fe401159c61a03df7ecef3f96b754344
nishant-suggaa <nishant@suggaa.in>
2023-12-27 13:15:20 +0530

    Updating

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <h1>Hello</h1>
+      <body>
+        {children}
+      </body>
     </html>
   )
 }
